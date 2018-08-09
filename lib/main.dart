43f6c2db68557ca25417ff3f3f544e86ea1636a2
alexLuo <alexLuo@seeapp.com>
2018-08-09 12:29:50 +0800

    添加动画效果

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,14 @@ class ChatScreen extends StatefulWidget {
   State createState() => new ChatScreenState();
 }
 
-class ChatScreenState extends State<ChatScreen> {
+/*
+* Flutter中动画被封装为Animation：包含类型值和状态（例如前进，后退，完成和解除）的对象。
+* 基于对动画对象属性的更改，框架可以修改窗口小部件的显示方式并重建窗口小部件树。
+* 动画控制器AnimationController指定动画的运行方式，可以定义动画的重要特征
+* 创建AnimationController时需要将vsync参数传递给它，改参数可以防止动画在屏幕之外消耗不必要的资源
+* 如果要使用ChatScreenState的vsync，需要在ChatScreenState类定义中包含TickerProviderStateMixin的mixin
+*/
+class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   // 每个列表项都是一个ChatMessage的实例，初始化列表为空
   final List<ChatMessage> _messages = <ChatMessage>[];
   // TextEditingController读取输入字段的内容，并在发送文本消息后清除字段
@@ -31,13 +38,26 @@ class ChatScreenState extends State<ChatScreen> {
   void _handleSubmitted(String text) {
     _textController.clear();
     ChatMessage message = new ChatMessage(
-      text: text,
-    );
+        text: text,
+        // 实例化一个AnimationController对象并将动画的运行时间指定为700毫秒。
+        animationController: new AnimationController(
+          duration: new Duration(milliseconds: 700),
+          vsync: this,
+        ));
     // setState是一个同步操作
     // 通常，setState()在此方法调用之外更改某些私有数据后，可以使用空闭包进行调用。但是，更新内部setState()闭包中的数据是首选，因此您不必忘记之后调用它。
     setState(() {
       _messages.insert(0, message);
     });
+    message.animationController.forward();
+  }
+
+  @override
+  // 重写dispose返回，达到处动画理控制器时候不在需要释放资源，担忧多个屏幕的时候
+  void dispose() {
+    for (ChatMessage message in _messages)
+      message.animationController.dispose();
+    super.dispose();
   }
 
   @override
@@ -109,34 +129,42 @@ const String _name = "Your name";
 
 // 聊甜消息的小部件需要嵌套在父的可滚动列表中
 class ChatMessage extends StatelessWidget {
-  ChatMessage({this.text});
+  ChatMessage({this.text, this.animationController});
   final String text;
+  // 变量用于存储动画控制器
+  final AnimationController animationController;
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      margin: const EdgeInsets.symmetric(vertical: 10.0),
-      /*
-      * 对于头像，父级是一个Row小部件，其主轴是水平的，因此CrossAxisAlignment.start沿垂直轴给出最高位置。
-      * 对于消息，父级是一个Column小部件，其主轴是垂直的，因此CrossAxisAlignment.start沿着水平轴将文本对齐在最左边的位置。
-      */
-      child: new Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          new Container(
-            margin: const EdgeInsets.only(right: 16.0),
-            child: new CircleAvatar(child: new Text(_name[0])),
-          ),
-          new Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              new Text(_name, style: Theme.of(context).textTheme.subhead),
-              new Container(
-                margin: const EdgeInsets.only(top: 5.0),
-                child: new Text(text),
-              )
-            ],
-          )
-        ],
+    // 返回SizeTransition包装Container对子窗口小部件提供动画进出的效果
+    return new SizeTransition(
+      sizeFactor: new CurvedAnimation(
+          parent: animationController, curve: Curves.easeOut),
+      axisAlignment: 0.0,
+      child: new Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        /*
+        * 对于头像，父级是一个Row小部件，其主轴是水平的，因此CrossAxisAlignment.start沿垂直轴给出最高位置。
+        * 对于消息，父级是一个Column小部件，其主轴是垂直的，因此CrossAxisAlignment.start沿着水平轴将文本对齐在最左边的位置。
+        */
+        child: new Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Container(
+              margin: const EdgeInsets.only(right: 16.0),
+              child: new CircleAvatar(child: new Text(_name[0])),
+            ),
+            new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Text(_name, style: Theme.of(context).textTheme.subhead),
+                new Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: new Text(text),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
